0f25d453db01cd1e9084564df9f3241b162fff28
Jimmy Hodgson <jimmy.hodgson@outlook.com>
2021-03-21 21:18:51 -0600

    started work on table component

---
+++ BlogCore/assets/src/js/components/education-table.js
@@ -0,0 +1,17 @@
+﻿if (document.getElementById('education-table') !== null) {
+    new Vue({
+        el: '#education-table',
+        data: function () {
+            return {
+                endpoint: '',
+                data: [],
+                success:true
+            };
+        },
+        methods: {
+            isEmpty() {
+                return !this.data.length > 0;
+            }
+        }
+    });
+}
